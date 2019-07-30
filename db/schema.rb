7beb5ef4a81e654b4eae5be591ee5339d352360e
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-30 14:42:34 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20190730131428) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password_digest"
+    t.string "password"
   end
 
 end
