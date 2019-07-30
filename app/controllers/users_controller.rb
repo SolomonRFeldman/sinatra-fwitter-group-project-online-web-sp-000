3b77d16b32ac6f9bd57a495937c37d29b9756f18
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-30 14:22:50 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
       param[1] == "" }
       redirect '/signup'
     end
+    binding.pry
     @user = User.create(params[:user])
     session[:user_id] = @user.id
     redirect "/tweets"
