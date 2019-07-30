c0c85d975f022b817252a9629dce7dc41d430fec
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-30 17:19:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    session_secret
+    set :session_secret, 
   end
   
   get '/' do
