f0f2e57a2164c805579ab9060fe2047308f6c19c
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-30 15:14:46 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,8 @@ class UsersController < ApplicationController
   end
   
   post '/login' do
-    
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password_digest])
   end
   
     
